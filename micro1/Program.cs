fb32f3a1ab50805fe4690b099e15a7c74a967c58
Aremchik <artema.1605@gmail.com>
2025-05-11 22:46:00 +0300

    wip

--- micro1/Program.cs
+++ micro1/Program.cs
@@ -3,40 +3,34 @@ using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Добавляем сервисы Swagger
+// Конфигурация сервисов
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
 });
 
-builder.Services.AddControllers();
+// Настройка БД
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
-// Настройка Swagger middleware
-if (app.Environment.IsDevelopment())
+// Middleware pipeline
+app.UseSwagger();
+app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1"));
+
+// Отключаем HTTPS редирект в Development
+if (!app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI(c => 
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
-    });
-    
-    // Отключаем HTTPS редирект в development
-    app.Logger.LogInformation("Running in Development mode - HTTPS redirection disabled");
-}
-else
-{
-    // Включаем HTTPS редирект в production
     app.UseHttpsRedirection();
 }
 
 app.UseAuthorization();
 app.MapControllers();
 
+// Инициализация БД
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
