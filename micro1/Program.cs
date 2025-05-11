0e1c2d977922e32d74b536d4d9d7f510ef9d8746
Aremchik <artema.1605@gmail.com>
2025-05-11 22:00:09 +0300

    wip

--- micro1/Program.cs
+++ micro1/Program.cs
@@ -1,14 +1,13 @@
-using Microsoft.EntityFrameworkCore;
-using csharp_service.Models; // Добавьте это
-
 var builder = WebApplication.CreateBuilder(args);
 
+// Добавьте сервисы в контейнер
 builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
+// Настройка конвейера HTTP-запросов
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
@@ -18,6 +17,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
 
+// Инициализация БД
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
