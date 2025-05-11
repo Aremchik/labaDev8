c0bbcd5a74456741fc821b5a330d185679138cb2
Aremchik <artema.1605@gmail.com>
2025-05-11 22:24:42 +0300

    wip

--- micro1/Program.cs
+++ micro1/Program.cs
@@ -33,4 +33,7 @@ app.MapControllers();
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    db.Database.Ensure
+    db.Database.EnsureCreated();
+}
+
+app.Run();
