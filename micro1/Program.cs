10b71283f4e4c4bfdaa41a32beff92b8d224b98a
Aremchik <artema.1605@gmail.com>
2025-05-11 21:55:12 +0300

    wip

--- micro1/Program.cs
+++ micro1/Program.cs
@@ -17,7 +17,6 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
 
-\
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
